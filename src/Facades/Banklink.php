d6d0d0af297ced0d51668c5d954ffb785117587f
William Trevisan <williamtrev@hotmail.com>
2025-09-17 19:03:53 -0300

    feat(itau): implement itau support

--- src/Facades/Banklink.php
+++ src/Facades/Banklink.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Banklink\Contracts\Bank authenticate(string $token)
- * @method static \Banklink\Entities\Account account()
- * @method static \Banklink\Entities\Card card(string $name)
  */
 final class Banklink extends Facade
 {
