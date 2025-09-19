c4b0dc2ff04898319b39a1607b11a4c86eb46475
William Trevisan <williamtrev@hotmail.com>
2025-09-19 11:25:07 -0300

    feat(itau): implement itau support

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,14 +2,38 @@
 
 declare(strict_types=1);
 
+use Banklink\Facades\Banklink;
 use Brick\Math\BigNumber;
 use Brick\Money\Money;
+
+if (! function_exists('banklink')) {
+    /**
+     * Get a new banklink proxy object from the given token.
+     *
+     * @param string|null $token
+     * @return ($string is null ? object : Banklink)
+     */
+    function banklink(?string $token = null)
+    {
+        if (func_num_args() === 0) {
+            return new class
+            {
+                public function __call($method, $parameters)
+                {
+                    return Banklink::$method(...$parameters);
+                }
+            };
+        }
+
+        return Banklink::authenticate($token);
+    }
+}
 
 if (! function_exists('money')) {
     /**
      * Get a new moneyable object from the given string.
      *
-     * @param  string|null  $string
+     * @param string|null  $string
      * @return ($string is null ? object : Money)
      */
     function money(BigNumber|int|float|string|null $amount = null)
