6f6fab340bab3c31eaec4fc37e0405087bb4b7b7
William Trevisan <williamtrev@hotmail.com>
2025-10-05 23:24:34 -0300

    feat(cache): remove cache to bank connection

--- src/Banklink.php
+++ src/Banklink.php
@@ -5,27 +5,13 @@ declare(strict_types=1);
 namespace Banklink;
 
 use Banklink\Contracts\Bank;
-use Illuminate\Support\Facades\Cache;
-use InvalidArgumentException;
 
 final readonly class Banklink
 {
     public function __construct(private Bank $bank) {}
 
-    public function authenticate(?string $token = null): Bank
+    public function authenticate(string $token): Bank
     {
-        $bank = config()->get('banklink.bank');
-        $agency = config()->get("banks.$bank.agency");
-        $account = config()->get("banks.$bank.account");
-
-        if (! cache()->has($key = "banklink.$bank.$agency.$account") && is_null($token)) {
-            throw new InvalidArgumentException('The token parameter is required.');
-        }
-
-        return Cache::remember(
-            $key,
-            ttl: now()->addMonth(),
-            callback: fn () => $this->bank->authenticate($token),
-        );
+        return $this->bank->authenticate($token);
     }
 }
